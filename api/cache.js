12865f1ff93141230ccc2a2913b0ad699ba3cd54
pietupai <pietupai@gmail.com>
2025-02-22 13:33:07 +0200

    Update cache.js

--- api/cache.js
+++ api/cache.js
@@ -1,7 +1,10 @@
 let cache = {};
 
 module.exports = {
-  getCache: () => cache,
+  getCache: () => {
+    console.log('Getting cache:', cache); // Logging to track cache retrieval
+    return cache;
+  },
   setCache: (newCache) => {
     cache = newCache;
     console.log('Cache updated:', cache); // Logging to track cache updates
