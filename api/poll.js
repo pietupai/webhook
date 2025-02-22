2423dacc13dd66ec47049a2c3879995b6be90aa8
pietupai <pietupai@gmail.com>
2025-02-22 13:09:17 +0200

    Update poll.js

--- api/poll.js
+++ api/poll.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { getCache } = require('./cache');
 
 const app = express();
 
 app.get('/api/poll', (req, res) => {
-  if (!cache || !cache.content) {
+  const cache = getCache();
+  if (!cache.content) {
     return res.status(404).send('No data available');
   }
   res.status(200).send(cache);
