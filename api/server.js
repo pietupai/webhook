aa2664c0d7063c62266e38481a72152322f4fcf4
pietupai <pietupai@gmail.com>
2025-02-22 14:29:58 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -58,11 +58,6 @@ app.get('/api/sse', (req, res) => {
   });
 });
 
-// Serve the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
