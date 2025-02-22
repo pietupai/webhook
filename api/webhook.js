b7fcc5c5b778061e12f5f39b3bd9a79e6eca0243
pietupai <pietupai@gmail.com>
2025-02-22 13:08:49 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { setCache } = require('./cache');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-let cache = {}; // In-memory cache
 
 app.post('/api/webhook', (req, res) => {
   const body = req.body;
   console.log('Webhook event received:', body);
 
   // Store the content in the cache
-  cache = { content: body };
+  setCache({ content: body });
 
   res.status(200).send('Webhook received');
 });
