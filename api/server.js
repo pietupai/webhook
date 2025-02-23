cdb642d50ba23717364799eff7dc7123be661913
pietupai <pietupai@gmail.com>
2025-02-23 06:27:37 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-//const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 //const events = require('events');
 
 const app = express();
