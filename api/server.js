7986a39c8b382d616c7decdace9ac20b36a9725e
pietupai <pietupai@gmail.com>
2025-02-23 07:09:10 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,40 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-//const fetch = require('node-fetch');
-//const events = require('events');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
 let cache = {}; // In-memory cache
 
-//const eventEmitter = new events.EventEmitter();  
-
-// Serve static files : tarvitaan että index.html toimii myös local
-//app.use(express.static(path.join(__dirname, '../public')));   // from 'public' directory
-app.use(express.static(path.join(__dirname, '../')));        // from root     
-
 app.post('/api/webhook', (req, res) => {
-//app.post('/api/webhook', async (req, res) => {
   const body = req.body;
   console.log('Webhook event received:', body);
 
   // Store the content in the cache
   cache = { content: body };
   console.log('Cache updated:', cache); // Logging to track cache updates
-
-  // Fetch the updated response.txt content
-  //const response = await fetch('https://api.github.com/repos/pietupai/hae/contents/response.txt');
-  //const data = await response.json();
-  //const decodedContent = Buffer.from(data.content, 'base64').toString('utf8');
-
-  // Emit event with the updated content
-  console.log('Emitting event: newWebhook');
-  //const decodedContent = Buffer.from(body, 'base64').toString('utf8');
-  //eventEmitter.emit('newWebhook', decodedContent);
-  //eventEmitter.emit('newWebhook', "testi123");
 
   res.status(200).send('Webhook received');
 });
@@ -48,27 +31,6 @@ app.get('/api/poll', (req, res) => {
     return res.status(200).send({ message: 'No data available' });
   }
   res.status(200).send(cache);
-});
-
-// SSE endpoint
-app.get('/api/sse', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  console.log('SSE connection established');
-
-  const keepAlive = setInterval(() => { res.write(': keep-alive\n\n'); console.log('Keep-alive message sent'); }, 15000);
-
-  //const listener = (data) => { console.log('Sending data to SSE client:', data); res.write(`data: ${data}\n\n`);  };
-
-  //eventEmitter.on('newWebhook', listener);
-
-  req.on('close', () => {
-    clearInterval(keepAlive);
-    //eventEmitter.removeListener('newWebhook', listener);
-    console.log('SSE connection closed');
-  });
 });
 
 const PORT = process.env.PORT || 3000;
